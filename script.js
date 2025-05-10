acd34c136f0bf58d3ecf68844eec74e0a23c57c2
Ali-Mazhar <syeadmuhammedalimazhar@gmail.com>
2025-05-10 08:44:03 -0700

    ch 5 to ch 7 code updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 // _____________8-08-2025 starts______________
 
+// chapter 01 = Alerts____________
+
 // program 1
 // 1. Write a script to greet your website visitor using JS alert box.
 alert("Welcome to script.js!");
@@ -27,6 +29,7 @@ console.log("Hello... I can run JS through my web browser's console");
 // program 7
 // Variable, Strings & Numbers
 
+
 // 1. Declare a variable called username
 var username;
 
@@ -37,8 +40,19 @@ var muName = "Syed Muhammad Ali";
 // a. declare a JS variable, titled message
 // b. assign “Hello World” to the variable message
 // c. display the message in alert box
+
+// chapter 2 = variable for strings____________
+
+
 var message = "Hello World";
 alert(message);
+
+
+//  chapter 3 = variable for numbers____________
+
+var age = 19;
+console.log(age);
+
 
 // 4. Write a script to save student’s bio data in JS variables and
 // show the data in console (Name, Age, Institute, Course)
@@ -137,11 +151,94 @@ alert("I am trying to learn from the Book " + book);
 
 // _____________10-05-2025 starts______________
 
+// chapter 05 = Familiar operators_______________
+
+// alerts are automatically add and subtract
+// program 1   
+alert(2 + 2);
+alert(4 - 2);
+
+// program 2
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+console.log(popularNumber); //11
+
+
+// chapter 06 = Unfamiliar operators_______________
+
+// program 1
+// num = 2
+// pre increment ++num  (++num = 3) ya direct + karaga 
+// post increment num++  (num++ = 2, num = 3)  ya next call ma + karaga
+
+
+var num1 = 10;
+var result1 = num1 + num1++ + ++num1;
+//  result1 = 10 + 10 + 12 = 32
+console.log(result1); // 32
+
+
+var num2 = 10;
+var result2 = num2 + ++num2 + num1++;
+//  result2 = 10 + 11 + 11 = 32
+console.log(result1); // 32
+
+
+// program 2
+// num = 2
+// pre decrement --num  (--num = 1) ya direct - karaga 
+// post decrement num-- (num-- = 2, num = 1)  ya next call ma - karaga
+
+var num1 = 2;
+var result1 = num1 + num1-- + --num1;
+//  result1 = 2 + 2 + 0 = 4
+console.log(result1); // 4
+
+
+var num2 = 2;
+var result2 = num2 + --num2 + num1--;
+//  result2 = 10 + 11 + 11 = 32
+console.log(result1); // 4
+
+
+// program 3
+
+
+var a = 5;
+var b = a++ + ++a + a + --a + a--;
+//  5 + 7 + 7 + 6 + 6
+console.log(b); // 31
+
+var x = 3;
+var y = x++ + ++x + x-- + x + --x;
+// 3 + 5 + 5 + 4 + 3
+console.log(y); // 20
+
+
+var c = 4;
+var d = c++ + c++ + ++c + c;
+// 4 + 5 + 7 + 7
+console.log(d); // 23
+
+
+var e = 5;
+var f = e-- + ++e + e++ + --e + e;
+// 5 + 5 + 5 + 5 + 5
+console.log(f); // 25
+
+
+var a = 3;
+var b = ++a + a++ + a + --a + a-- + a;
+// 4 + 4 + 5 + 4 + 4 + 3
+console.log(b); // 24
 
 
 
+// chapter 07 = Eliminating ambiguity _______________
 
-
+var totalCost = 1 + 3 * 4;
+console.log(totalCost); // 13
 
 
 // _____________10-05-2025 starts______________
